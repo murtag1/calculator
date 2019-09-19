74be26ba84a7362842850204564802bb621568d8
murtag1 <murtag97@mail.ru>
2019-09-19 20:58:19 +0700

    Added castom validation messages

--- src/js/FormView.js
+++ src/js/FormView.js
@@ -15,21 +15,20 @@ class FormView {
 
    _handleSubmit(event) {
       event.preventDefault();
-      this.errorElement.classList.remove('calculator__error_visible');
 
-      const creditAmount = this._validateNumInput(this.creditAmountElement.value);
-      const creditTerm = this._validateNumInput(this.creditTermElement.value);
-      const creditInterestRate = this._validateNumInput(this.creditInterestRateElement.value);
-      const firstPaymentDate = this._validateDateInput(this.firstPaymentDateElement.value);
+      this._removeErrorElement();
+
+      const creditAmount = this._validateNumInput(this.creditAmountElement);
+      const creditTerm = this._validateNumInput(this.creditTermElement);
+      const creditInterestRate = this._validateNumInput(this.creditInterestRateElement);
+      const firstPaymentDate = this._validateDateInput(this.firstPaymentDateElement);
 
       const typePayment = this.typePaymentElement.value;
 
       const isInputsValidate = !creditAmount || !creditTerm
             || !creditInterestRate || !firstPaymentDate;
-      if (isInputsValidate) {
-         this.errorElement.classList.add('calculator__error_visible');
-         return;
-      }
+
+      if (isInputsValidate) return;
 
       creditCalc.calcCreditInfo({
          creditAmount,
@@ -40,27 +39,64 @@ class FormView {
       });
    }
 
-   _validateNumInput(inputValue) {
-      if (inputValue === '') return false;
+   _validateNumInput(inputElement) {
+      const inputValue = inputElement.value;
+      if (inputValue === '') {
+         this._showError(inputElement, 'Введите число');
+         return false;
+      }
+
       const numValue = Number(inputValue);
       const isValueInvalidate = Number.isNaN(numValue) || numValue < 0;
       if (isValueInvalidate) {
+         this._showError(inputElement, 'Введите число');
          return false;
       }
       return numValue;
    }
 
-   _validateDateInput(inputValue) {
-      // eslint-disable-next-line no-param-reassign
+   _validateDateInput(inputElement) {
+      let inputValue = inputElement.value;
       inputValue = inputValue.replace(/\s/g, '');
 
-      if (inputValue === '') return false;
       const dateValue = inputValue.match(/^(1[0-2]|0[1-9])\.(1[0-2]|0[1-9])\.[0-9]{4}$/ig);
 
       if (!dateValue) {
+         this._showError(inputElement, 'Введите дату в формате "дд.мм.гггг"');
          return false;
       }
       return inputValue;
+   }
+
+   _showError(inputElement, message) {
+      if (this.errorElement) return;
+      const inputWrapper = inputElement.parentNode;
+
+      this.inputWithError = inputElement;
+      this.errorElement = document.createElement('div');
+      this.errorElement.className = 'calculator__error';
+      this.errorElement.textContent = message;
+
+      inputWrapper.appendChild(this.errorElement);
+      inputElement.focus();
+
+      this.removeErrorTimer = setTimeout(() => {
+         this._removeErrorElement();
+      }, 2000);
+
+      // eslint-disable-next-line no-param-reassign
+      inputElement.onblur = () => {
+         this._removeErrorElement();
+      };
+   }
+
+   _removeErrorElement() {
+      if (this.errorElement) {
+         this.errorElement.remove();
+         this.errorElement = null;
+         this.inputWithError.onblur = null;
+         clearTimeout(this.removeErrorTimer);
+      }
    }
 
    _addEventListeners() {
